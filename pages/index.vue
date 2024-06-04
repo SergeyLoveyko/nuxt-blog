34f2268f17d5a2503c1e2a2de75a968cadb6a43a
Sergey <loveykosergey@ukr.net>
2024-06-05 00:12:00 +0300

    feat: add main page

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,24 @@
 <template>
-  <Tutorial/>
+  <!-- <Tutorial/> -->
+  <el-row type="flex" justify="center">
+    <el-col :xs="24" :sm="18" :md="12" :lg="10">
+      <app-post 
+        v-for="post in 3"
+        :key="post"
+      />
+    </el-col>
+  </el-row>
 </template>
 
 <script>
+import AppPost from '@/components/main/Post.vue'
+
 export default {
-  name: 'IndexPage'
+  head: {
+    title: "Головна"
+  },
+  components: {
+    AppPost
+  }
 }
 </script>
