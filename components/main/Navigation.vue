5dd34e5311f476e413a70e4dd07938a5c559b838
Sergey <loveykosergey@ukr.net>
2024-06-05 00:09:30 +0300

    feat: create layout

---
+++ components/main/Navigation.vue
@@ -0,0 +1,9 @@
+<template>
+  <nav>
+    <h1>SSR Blog</h1>
+  </nav>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,32 @@
+<template>
+  <el-container>
+    <el-header>
+      <app-navigation />
+    </el-header>
+    <el-main>
+      <nuxt />
+    </el-main>
+    <el-footer />
+  </el-container>
+</template>
+
+<script>
+import AppNavigation from '@/components/main/Navigation.vue'
+
+export default {
+  components: {
+    AppNavigation
+  }
+}
+
+</script>
+
+<style>
+  nav {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    border-bottom: 1px solid #e6e6e6;
+  }
+</style>
