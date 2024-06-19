c3f5006b1ce539afe03279365eb3e0f9246e32aa
Sergey <loveykosergey@ukr.net>
2024-06-20 00:25:03 +0300

    feat: add user authorization

--- server/keys/keys.dev.js
+++ server/keys/keys.dev.js
@@ -1,3 +1,4 @@
 module.exports = {
-  MONGO_URI: 'mongodb+srv://demo-user:q123456@cluster0.v6rwsjc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+  MONGO_URI: 'mongodb+srv://demo-user:q123456@cluster0.v6rwsjc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+  JWT: 'dev-jwt-key'
 }
